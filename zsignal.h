2302e768a32825e33180c3cff239d0f0834540ac
RykerVa <135276174+RykerVa@users.noreply.github.com>
2024-09-24 10:49:59 -0500

    Update zsignal.h

---
+++ zsignal.h
@@ -0,0 +1,16 @@
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+void handle_ctrlz(int);
+
+#endif
